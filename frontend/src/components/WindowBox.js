7b1c112222a9936a5c46565b7f1d6840426d8d75
Paul Phukon <hello@snehanshu.tech>
2020-11-07 02:27:45 +0530

    ability to add and update data

--- frontend/src/components/WindowBox.js
+++ frontend/src/components/WindowBox.js
@@ -1,7 +1,8 @@
-import { get, find } from "lodash-es";
+import { get, find, findIndex } from "lodash-es";
 import styled from "styled-components";
 import { DragHandleHorizontal, DragHandleVertical } from "./Handles";
 import store from '../store';
+import axios from '../axios';
 
 const Win = styled.div`
   display: inline-block;
@@ -25,11 +26,56 @@ export default function WindowBox({
   const loading = store.useState(s => s.contentLoading);
   const content = store.useState(s => get(find(s.contents, { windowId }), 'content'));
 
+  const addContent = () => {
+    const newContent = prompt('Write something interesting...');
+
+    if (newContent) {
+      axios.post('/add', {
+        windowId,
+        content: newContent
+      }).then(() => store.update(s => {
+        const index = findIndex(s.contents, { windowId });
+        if (index > -1) {
+          s.contents[index].content = newContent;
+        } else {
+          s.contents.push({ windowId, content: newContent });
+        }
+
+        s.counter.add += 1;
+      }))
+      .catch(err => alert(err.message));
+    }
+  };
+
+  const updateContent = () => {
+    const updatedContent = prompt('Write something interesting...', content);
+
+    if (updatedContent) {
+      axios.put('/update', {
+        windowId,
+        content: updatedContent
+      }).then(() => store.update(s => {
+        const index = findIndex(s.contents, { windowId });
+        if (index > -1) {
+          s.contents[index].content = updatedContent;
+        }
+
+        s.counter.update += 1;
+      }))
+      .catch(err => alert(err.message));
+    }
+  };
+
   return <Win height={height} width={width}>
     <div>
       {loading ? <b>Loading...</b> : (
         content ? <span>{content}</span> : <b>No content</b>
       )}
+    </div>
+
+    <div style={{ marginTop: '10px' }}>
+      <button onClick={addContent}>Add content</button>
+      {content && <button onClick={updateContent}>Update content</button>}
     </div>
 
     <DragHandleHorizontal
