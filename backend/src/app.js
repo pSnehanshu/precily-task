7b109a5d62b781801c8bb5491b0a4e5e43e2ce97
Paul Phukon <hello@snehanshu.tech>
2020-11-07 01:27:18 +0530

    fetch counters

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = require('express')();
+const dbPromise = require('./db');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get('/count', (req, res) => dbPromise
+  .then((db) => db.get('SELECT * FROM counter'))
+  .then((data) => res.json(data)));
 
 module.exports = app;
